896ee5c88cd18564e4ba90cc99c2b3661316d7ac
AngelG2903 <angel.gabriel-1999@hotmail.com>
2024-11-10 19:48:34 -0600

    MedicalForm 80%

--- application/controllers/medicalFormController.js
+++ application/controllers/medicalFormController.js
@@ -72,46 +72,46 @@ exports.update = async (req, res) => {
     } = req.body;
 
     try {
-        const medicalHis = await MedicalHistory.findOne({ where: { patientId: id } });
+        const medicalHis = await MedicalHistory.findOne({ where: { id } });
         if (!medicalHis) {
             return res.status(404).json({ error: 'Medical record not found' });
         }
 
-        await MedicalHistory.update({ weight, size, tA, fC, fR, t, history1, history2, history3, history4, history5, history6, history7, history8 }, { where: { patientId: id } });
+        await MedicalHistory.update({ weight, size, tA, fC, fR, t, history1, history2, history3, history4, history5, history6, history7, history8 }, { where: { id } });
 
         // valida Cardiovascular
         const hasCardiovascularData = [cardiovascular1, cardiovascular2, cardiovascular3, cardiovascular4, cardiovascular5, cardiovascular6].some(value => value !== null && value !== undefined && value !== '');
-        const existingCardio = await CardiovascularSystem.findOne({ where: { medicalId: medicalHis.id } });
+        const existingCardio = await CardiovascularSystem.findOne({ where: { medicalId: id } });
         if (existingCardio) {
-            await CardiovascularSystem.update({ cardiovascular1, cardiovascular2, cardiovascular3, cardiovascular4, cardiovascular5, cardiovascular6 }, { where: { medicalId: medicalHis.id } });
+            await CardiovascularSystem.update({ cardiovascular1, cardiovascular2, cardiovascular3, cardiovascular4, cardiovascular5, cardiovascular6 }, { where: { medicalId: id } });
         } else if (hasCardiovascularData) {
-            await CardiovascularSystem.create({ medicalId: medicalHis.id, cardiovascular1, cardiovascular2, cardiovascular3, cardiovascular4, cardiovascular5, cardiovascular6 });
+            await CardiovascularSystem.create({ medicalId: id, cardiovascular1, cardiovascular2, cardiovascular3, cardiovascular4, cardiovascular5, cardiovascular6 });
         }
 
         // valida ETS
         const hasEtsDiseaseData = [disease1, disease2, disease3, disease4].some(value => value !== null && value !== undefined && value !== '');
-        const existingEts = await EtsDisease.findOne({ where: { medicalId: medicalHis.id } });
+        const existingEts = await EtsDisease.findOne({ where: { medicalId: id } });
         if (existingEts) {
-            await EtsDisease.update({ disease1, disease2, disease3, disease4 }, { where: { medicalId: medicalHis.id } });
+            await EtsDisease.update({ disease1, disease2, disease3, disease4 }, { where: { medicalId: id } });
         } else if (hasEtsDiseaseData){
-            await EtsDisease.create({ medicalId: medicalHis.id, disease1, disease2, disease3, disease4 });
+            await EtsDisease.create({ medicalId: id, disease1, disease2, disease3, disease4 });
         }
 
         // validar PathologicalHistory
         const hasPathologicalHistoryData = [colitis, gastritis, gastroenteritis, asma, bronquitis, neumonia, tuberculosis, farinoamigdalitis, pathological1, pathological2, pathological3, pathological4, pathological5, pathological6, pathological7].some(value => value !== null && value !== undefined && value !== '');
-        const existingPathological = await PathologicalHistory.findOne({ where: { medicalId: medicalHis.id } });
+        const existingPathological = await PathologicalHistory.findOne({ where: { medicalId: id } });
         if (existingPathological) {
-            await PathologicalHistory.update({ colitis, gastritis, gastroenteritis, asma, bronquitis, neumonia, tuberculosis, farinoamigdalitis, pathological1, pathological2, pathological3, pathological4, pathological5, pathological6, pathological7 }, { where: { medicalId: medicalHis.id } });
+            await PathologicalHistory.update({ colitis, gastritis, gastroenteritis, asma, bronquitis, neumonia, tuberculosis, farinoamigdalitis, pathological1, pathological2, pathological3, pathological4, pathological5, pathological6, pathological7 }, { where: { medicalId: id } });
         } else if (hasPathologicalHistoryData) {
-            await PathologicalHistory.create({  medicalId: medicalHis.id, colitis, gastritis, gastroenteritis, asma, bronquitis, neumonia, tuberculosis, farinoamigdalitis, pathological1, pathological2, pathological3, pathological4, pathological5, pathological6, pathological7 });
+            await PathologicalHistory.create({  medicalId: id, colitis, gastritis, gastroenteritis, asma, bronquitis, neumonia, tuberculosis, farinoamigdalitis, pathological1, pathological2, pathological3, pathological4, pathological5, pathological6, pathological7 });
         }
 
         const hasOralCavityData = [cavity1, cavity2, cavity3, dolor, luxacion, anquilosis, crepitacion, subluxacion, espasmoMuscular].some(value => value !== null && value !== undefined && value !== '');
-        const existingOralCavity = await OralCavity.findOne({ where: { medicalId: medicalHis.id } });
+        const existingOralCavity = await OralCavity.findOne({ where: { medicalId: id } });
         if (existingOralCavity) {
-            await OralCavity.update({ cavity1, cavity2, cavity3, dolor, luxacion, anquilosis, crepitacion, subluxacion, espasmoMuscular }, { where: { medicalId: medicalHis.id } });
+            await OralCavity.update({ cavity1, cavity2, cavity3, dolor, luxacion, anquilosis, crepitacion, subluxacion, espasmoMuscular }, { where: { medicalId: id } });
         } else if (hasOralCavityData) {
-            await OralCavity.create({ medicalId: medicalHis.id, cavity1, cavity2, cavity3, dolor, luxacion, anquilosis, crepitacion, subluxacion, espasmoMuscular });
+            await OralCavity.create({ medicalId: id, cavity1, cavity2, cavity3, dolor, luxacion, anquilosis, crepitacion, subluxacion, espasmoMuscular });
         }
 
         res.status(200).json({ message: 'Medical record updated successfully' });
@@ -156,13 +156,13 @@ exports.getMedicalFormById = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const patient = await Patient.findOne({ where: { id }, include: [MedicalHistory, CardiovascularSystem, EtsDisease, PathologicalHistory, OralCavity] });
-        const medical = await MedicalHistory.findOne({ where: { patientId: id} })
-        if (!patient || !medical) {
+        const medical = await MedicalHistory.findOne({ where: { id }, include: [CardiovascularSystem, EtsDisease, PathologicalHistory, OralCavity] });
+        
+        if (!medical) {
             return res.status(404).json({ error: 'Medical record not found' });
         }
 
-        res.status(200).json(patient);
+        res.status(200).json(medical);
 
     } catch (error) {
         res.status(500).json({ error: 'server error', details: error.message });
@@ -179,10 +179,10 @@ exports.deleteExpe = async (req, res) => {
         }
 
         await MedicalHistory.destroy({ where: { id } });
-        await CardiovascularSystem.destroy({ where: { id } });
-        await EtsDisease.destroy({ where: { id } });
-        await PathologicalHistory.destroy({ where: { id } });
-        await OralCavity.destroy({ where: { id } });
+        await CardiovascularSystem.destroy({ where: { medicalId: id } });
+        await EtsDisease.destroy({ where: { medicalId: id } });
+        await PathologicalHistory.destroy({ where: { medicalId: id } });
+        await OralCavity.destroy({ where: { medicalId: id } });
 
         res.status(200).json({ message: 'Medical record deleted successfully' });
 
--- presentation/routes/medicalFormRoutes.js
+++ presentation/routes/medicalFormRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { register, update, deleteExpe, getAllMedicalForm, getMedicalFormById } = require('../../application/controllers/medicalFormController');
 const verifyToken = require('../../infrastructure/middlewares/verifyToken');
+const upload = require('../../infrastructure/middlewares/multerConfig');
 
 const router = express.Router();
 
-router.post('/register/:id', register);
+router.post('/register/:id', upload.none(), register);
 router.get('/get/:id', getAllMedicalForm);
-router.get('/gett/:id', getMedicalFormById);
-router.put('/update/:id', update);
+router.get('/getExam/:id', getMedicalFormById);
+router.put('/update/:id', upload.none(), update);
 router.delete('/delete/:id', deleteExpe);
 
 module.exports = router;
